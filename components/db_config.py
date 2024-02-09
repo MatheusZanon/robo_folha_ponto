de4c76c2021d276493ea5db977641c5074a8712c
MatheusZanon <52750182+MatheusZanon@users.noreply.github.com>
2024-02-09 14:58:33 -0300

    feat: adicionando componentes novos

--- components/db_config.py
+++
@@ -1,8 +0,0 @@
-def db_config():
-    db_config = {
-        'host': 'localhost',
-        'user': 'root',
-        'password': 'senhaBD',
-        'database': 'nomeBD'
-    }
-    return db_config
--- components/importacao_automacao_excel.py
+++
@@ -1,3 +0,0 @@
-"""IMPORTAÇÕES PARA AUTOMAÇÃO DE PLANILHAS COM EXCEL"""
-def auto_excel():
-    print("olá otario, estou aqui para automatizar seu excel")
